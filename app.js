ae88870e27c8533d18f8991f03b497fca3b9dcea
Freddy Castillo <freddy.geovanni@gmail.com>
2017-11-13 07:58:06 -0500

    add nodemon

--- app.js
+++ app.js
@@ -3,25 +3,25 @@ var express = require("express");
 
 var app = express();
 var User = require("./models/user").User;
-var session=require("express-session");
-var router_app=require("./rutas_app.js");
-var session_middleware=require("./middlewares/session.js");
+var session = require("express-session");
+var cookieSession = require("cookie-session");
+var router_app = require("./rutas_app.js");
+var session_middleware = require("./middlewares/session.js");
 
 var bodyParcer = require("body-parser")
 app.use("/docs", express.static("public"));
 app.use(bodyParcer.json());
 app.use(bodyParcer.urlencoded({ extended: true }));
-app.use(session({
-    secret:"hjkhsjfhsdhfkhf",
-    resave:true,
-    saveUninitialized:false
+app.use(cookieSession({
+    name: "session",
+    keys: ["llave-1", "llave-2"]
 }));
 
 app.set("view engine", "jade");
-app.get("/", function (req, res) { 
+app.get("/", function (req, res) {
     console.log(req.session.user_id);
     console.log(req.sessionID);
-    res.render("index", { usuario: "Freddy" }); 
+    res.render("index", { usuario: "Freddy" });
 });
 app.get("/login", function (req, res) {
 
@@ -52,17 +52,25 @@ app.post("/users", function (req, res) {
 
 });
 
+
 app.post("/sessions", function (req, res) {
     User.findOne({ email: req.body.email, password: req.body.password },
         function (err, doc) {
-            req.session.user_id=doc._id;
             console.log(doc);
-            console.log(req.session)
-            res.redirect("/app");
+            console.log(req.session);
+            console.log(err);
+            if (doc) {
+                req.session.user_id = doc._id;
+                console.log(doc);
+                console.log(req.session)
+                res.redirect("/app");
+            } else {
+                res.redirect("/signup");
+            }
         }
     );
 
 });
-app.use("/app",session_middleware);
-app.use("/app",router_app);
+app.use("/app", session_middleware);
+app.use("/app", router_app);
 app.listen(8080);
