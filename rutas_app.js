43a2907e5866719cd621f77be0625ac6e8d6dda7
Freddy Castillo <freddy.geovanni@gmail.com>
2017-11-13 21:57:45 -0500

    FCA - View images
    Signed-off-by: Freddy Castillo <freddy.geovanni@gmail.com>

--- rutas_app.js
+++ rutas_app.js
@@ -5,36 +5,44 @@ router.get("/", function (req, res) {
     res.render("app/home");
 });
 
-router.get("/imagenes/new",function(req,res){
+router.get("/imagenes/new", function (req, res) {
     res.render("app/imagenes/new")
 });
 router.route("/imagenes/:id").get(
     function (req, res) {
-        Imagen.findById(req.params.id,function(err,img){
-           console.log("Imagen Lectura");
+        Imagen.findById(req.params.id, function (err, img) {
+            console.log("Imagen Lectura");
             console.log(img);
-            if(err){
+            if (err) {
 
-            }else{
-                res.render("app/imagenes/show",{imagen:img});
+            } else {
+                res.render("app/imagenes/show", { imagen: img });
             }
         });
-        
-     }).put(function (req, res) { 
 
-     }).delete(function (req, res) { 
+    }).put(function (req, res) {
 
-     });
+    }).delete(function (req, res) {
+
+    });
 router.route("/imagenes").get(
     function (req, res) {
-    }).post(function (req, res) { 
-        var data={titulo:req.body.title};
-        var imagen= new Imagen(data);
+        Imagen.find({}, function (err, imagenes) {
+            if (err) {
+                res.redirect("/app");
+                return;
+            } else {
+                res.render("app/imagenes/index", { imagenes: imagenes });
+            }
+        });
+    }).post(function (req, res) {
+        var data = { titulo: req.body.title };
+        var imagen = new Imagen(data);
         console.log(imagen)
-        imagen.save(function(err){
-            if(!err){
-                res.redirect("/app/imagenes/"+imagen._id)
-            }else{
+        imagen.save(function (err) {
+            if (!err) {
+                res.redirect("/app/imagenes/" + imagen._id)
+            } else {
                 res.render(err);
             }
         });
