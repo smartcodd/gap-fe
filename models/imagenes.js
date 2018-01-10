272987cbae79646448a390f1841da031e0410346
Freddy Castillo <freddy.geovanni@gmail.com>
2018-01-10 17:18:07 -0500

    FCA  - Modificado Template

--- models/imagenes.js
+++ models/imagenes.js
@@ -1,11 +1,15 @@
 
-var mongoose=require("mongoose");
-var Schema= mongoose.Schema;
-mongoose.connect("mongodb://localhost:27017/gap");
-var img_schema= new Schema({
-    titulo:{type: String, require:true},
-    creator:{type:Schema.Types.ObjectId, ref:"User"},
-    extension:{type: String, require:true}
-});
-var Imagen=mongoose.model("Imagen",img_schema);
-module.exports.Imagen=Imagen;
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+try {
+    mongoose.connect("mongodb://localhost:27017/gap");
+    var img_schema = new Schema({
+        titulo: { type: String, require: true },
+        creator: { type: Schema.Types.ObjectId, ref: "User" },
+        extension: { type: String, require: true }
+    });
+    var Imagen = mongoose.model("Imagen", img_schema);
+    module.exports.Imagen = Imagen;
+} catch (error) {
+    console.log(error);
+}
